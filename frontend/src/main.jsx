474ec5b933b72c94268dd8503d868066f643b82f
ashwinisajjanshetti <ashwinisajjanshetti@gmail.com>
2025-10-16 23:08:15 +0530

    feat:added ntlify.toml file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/notfound" element={<AdminDashboard />} />
+        <Route path="/thinkpositive" element={<AdminDashboard />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
